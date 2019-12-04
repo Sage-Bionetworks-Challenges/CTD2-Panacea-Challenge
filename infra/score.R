1e49857deba2cd704354cf1e0c474f38bad60d43
verena <vpchung@users.noreply.github.com>
2019-12-04 13:16:27 -0800

    updating docker image for two test queues

--- infra/score.R
+++ infra/score.R
@@ -1,8 +1,6 @@
 library(argparse)
 library(rjson)
 
-source("/usr/local/bin/validation_scoring.R")
-
 parser = ArgumentParser()
 
 parser$add_argument(
@@ -35,8 +33,19 @@ parser$add_argument(
     type = "character",
     required = TRUE
 )
+parser$add_argument(
+    "--metric",
+    type = "character",
+    required = TRUE
+)
 
 args <- parser$parse_args()
+
+if (args$metric == "metric1") {
+    source("/usr/local/bin/validation_scoring.R")
+} else {
+    source("/usr/local/bin/validation_scoring_subset.R")
+}
 
 scores <- score(args$inputfile, args$goldstandard, 
     args$nullmodel1, args$nullmodel2, args$round)
